011202e999a6e9ff924d3147b32685cd724bca93
tim-liu-wzvtc <tim.liu@foxmail.com>
2021-12-19 22:00:28 +0800

    1 file modified, 1 added

--- src/components/Login.ts
+++ src/components/Login.ts
@@ -1,15 +1,5 @@
-export type LoginInfo = {
-  name: string;
-  password: string;
-};
-
-export class Login {
-  private data: LoginInfo;
-
-  constructor(data_: LoginInfo) {
-    this.data = data_;
-  }
-
+import { LoginBase } from './LoginBase';
+export class Login extends LoginBase {
   login(): void {
     if (this.data.name === '123' && this.data.password === '123') {
       window.location.href = './main.html';
