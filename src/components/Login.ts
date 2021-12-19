b5cf42a3b2a2784960a81da474b4ab762daa6840
tim-liu-wzvtc <tim.liu@foxmail.com>
2021-12-19 22:02:48 +0800

    2 files modified

--- src/components/Login.ts
+++ src/components/Login.ts
@@ -1,5 +1,5 @@
-import { LoginBase } from './LoginBase';
-export class Login extends LoginBase {
+import { LoginBase, ILogin } from './LoginBase';
+export class Login extends LoginBase implements ILogin {
   login(): void {
     if (this.data.name === '123' && this.data.password === '123') {
       window.location.href = './main.html';
--- src/components/LoginBase.ts
+++ src/components/LoginBase.ts
@@ -6,16 +6,10 @@ export interface ILogin {
   login(): void;
 }
 
-export class LoginBase implements ILogin {
+export class LoginBase {
   protected data: LoginInfo;
 
   constructor(data_: LoginInfo) {
     this.data = data_;
-  }
-
-  login(): void {
-    if (this.data.name === '123' && this.data.password === '123') {
-      window.location.href = './main.html';
-    }
   }
 }
