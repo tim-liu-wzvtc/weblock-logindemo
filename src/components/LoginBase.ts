db051850cf5982357dec25b713de3284239f7fd1
tim-liu-wzvtc <tim.liu@foxmail.com>
2021-12-22 20:40:02 +0800

    2 files modified

--- src/components/LoginBase.ts
+++ src/components/LoginBase.ts
@@ -16,3 +16,24 @@ export class LoginBase {
     this.data = data_;
   }
 }
+
+/*
+
+<script setup lang="ts">
+import RemoteComponent from '../RemoteComp.vue';
+import { ref, reactive } from 'vue';
+import type { LoginInfo } from './LoginBase';
+import { Login } from './Login';
+
+const data: LoginInfo = reactive({
+  name: '',
+  password: '',
+});
+
+let loginInstance = new Login(data);
+
+const login = loginInstance.login;
+</script>
+
+
+*/
