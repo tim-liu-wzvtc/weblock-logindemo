5bb619674b8d2c8d66e8901ce095c5162ef66108
tim-liu-wzvtc <tim.liu@foxmail.com>
2021-12-22 19:54:29 +0800

    1 file modified

--- src/components/Login.ts
+++ src/components/Login.ts
@@ -1,5 +1,5 @@
 import { LoginBase, ILogin } from './LoginBase';
-export {type LoginInfo} from './LoginBase'
+export { LoginInfo } from './LoginBase';
 export class Login extends LoginBase implements ILogin {
   login(): void {
     if (this.data.name === '123' && this.data.password === '123') {
@@ -7,4 +7,3 @@ export class Login extends LoginBase implements ILogin {
     }
   }
 }
-
